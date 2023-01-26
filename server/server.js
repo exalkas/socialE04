db8520451271702980dd15f779605ca7d48f8503
alkasdci <alkas.alkiviadis-freelancer@digitalcareerinstitute.org>
2023-01-26 14:28:43 +0200

    require fixed

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import userRoutes from './routes/userRoutes.js'
 import postRoutes from './routes/postRoutes.js'
 import db from './config/db.js'
 import cookieParser from 'cookie-parser'
+import path from 'path';
 
 dotenv.config()
 const app = express()
@@ -21,9 +22,7 @@ app.use(express.static('client/build'));
 
 if( process.env.NODE_ENV === 'production' ) {
 
-    const path = require('path');
-
-    app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
