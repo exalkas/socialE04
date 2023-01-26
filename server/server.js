f8f90bf763749639adee9235a85f04abfb0dc111
alkasdci <alkas.alkiviadis-freelancer@digitalcareerinstitute.org>
2023-01-26 14:20:05 +0200

    heroku changes applied

--- server/server.js
+++ server/server.js
@@ -16,3 +16,14 @@ app.use('/posts', postRoutes)
 
 const port = process.env.PORT || 4001
 app.listen(port, () => console.log('Server is up and running at port', port))
+
+app.use(express.static('client/build'));
+
+if( process.env.NODE_ENV === 'production' ) {
+
+    const path = require('path');
+
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    });
+}
